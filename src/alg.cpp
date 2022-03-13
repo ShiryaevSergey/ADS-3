fe86ba83dd229566ee54db3af77e77f2cc653745
puzhx <99177486+ShiryaevSergey@users.noreply.github.com>
2022-03-13 18:57:40 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,15 +5,21 @@ int l = 0;
 int r = size - 1;
 int mid = 0;
 int k = 0;
-    while (l <= r) {
-        mid = (l + r) / 2;
-        if (value == arr[mid]) {
-            k++;
-        } else if (value < arr[mid]) {
-            r = mid - 1;
-        } else {
-            l = mid + 1;
-        }
+  while (l < r) {
+    mid = (l + r) / 2;
+    if (value < arr[mid]) 
+      r = mid;
+    else 
+      l = mid + 1;   
+  }
+  if (arr[l] == value) {
+    while (arr[l] == value) {
+      k++;
+      l++;
     }
-    return k;  
+  }
+  if (k != 0)
+    return k;
+  else
+    return 0;      
 }
